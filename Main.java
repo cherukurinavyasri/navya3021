b6460033ed45d9a84d1aef91e888b018f2b332bc
navya30217 <59163481+navya30217@users.noreply.github.com>
2019-12-24 11:28:45 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,45 @@
+class Animal
+{
+   String  sleep()
+
+   {return ("sleeps..");}
+    String eat()
+
+    {return ("eats...");}
+
+
+}
+class Bird extends Animal
+{
+   String fly()
+
+    {return("flys...");}
+}
+
+
+class Main
+{
+  public static void main(String[] args)
+{
+   
+    Animal a=new Animal();
+   System.out.println("I"+a.eat());
+   System.out.println("I"+a.sleep());
+  
+   Bird b=new Bird();
+    System.out.println("I"+b.eat());
+   System.out.println("I"+b.sleep());
+   System.out.println("I"+b.fly());
+}
+}
+
+
+
+
+
+
+
+
+
+
+
