9c8353fe511c5140434e4dfecac954ac4d3c56e0
navya30217 <59163481+navya30217@users.noreply.github.com>
2019-12-24 09:28:17 +0530

    Add files via upload

---
+++ Four3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+class Four3
+{
+ public static void main(String[] args)
+ {
+    Scanner s=new Scanner(System.in);
+       String name=s.next();
+      double height=s.nextDouble();
+     double weight=s.nextDouble();
+
+     System.out.println(Patient.bmi(name,height,weight));
+ }
+}
+
+class Patient
+{
+   String name;
+   double weight,height;
+ double bmi(String name,double height,double weight)
+         {
+            return((weight/height*height)*703);
+
+        }
+}
+     
